73a257f56c0866b587418ab2018856719aae9940
Alessio Maffeis <alessiomaffeis@DW-MacBook-Pro.local>
2017-02-21 18:53:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# picture-input-example
+
+vue-picture-input repository: https://github.com/alessiomaffeis/vue-picture-input
+
+> PictureInput Example
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <picture-input ref="pictureInput" @change="onChange" name="profilePic" width="600" height="600" margin="16" accept="image/jpeg,image/png"></picture-input>  
+  </div>
+</template>
+
+<script>
+import PictureInput from 'vue-picture-input'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components: {
+    PictureInput
+  },
+  methods: {
+    onChange () {
+      console.log('New picture selected!')
+      if (this.$refs.pictureInput.image) {
+        console.log('Picture loaded.')
+      } else {
+        console.log('FileReader API not supported: use the <form>, Luke!')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
